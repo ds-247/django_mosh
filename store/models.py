2f7e8837cd06eddc758fc180209d2ca7027f97a7
Cipher <dipanshusaini69@gmail.com>
2024-03-31 20:23:40 +0530

    circular relationship problem

--- store/models.py
+++ store/models.py
@@ -71,3 +71,4 @@ class OrderItem (models.Model) :
 
 class Collection (models.Model) : 
     title = models.CharField(max_length = 255)
+    featured_product = models.ForeignKey('Product',  on_delete = models.SET_NULL, null =True, related_name = '+')
